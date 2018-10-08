d49e07ee727ffe0c9300d08dac5d669b815ac86a
Donie Ypon <donieypon@rauls-mbp-2>
2018-10-07 21:35:26 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Donie Ypon on 10/7/18.
+//  Copyright © 2018 Donie Ypon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
